15393d2d7f4d20acb4690591be21f8ae9f76aca0
Durga Prasad <durga@Himanshus-MacBook-Air.local>
2018-12-01 15:42:38 +0530

    First Commit After codeing

---
+++ Sample/NativeAlert.swift
@@ -0,0 +1,63 @@
+//
+//  NativeAlert.swift
+//  Sample
+//
+//  Created by Durga Prasad on 01/12/18.
+//  Copyright © 2018 Durga Prasad. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+typealias CompletionHandler = ((Int) -> Void)?
+//Button Titles----------------------
+
+enum AlertButtonTitle {
+    case ok, cancel, settings, yes, no, delete
+    
+    var value: String {
+        switch self {
+        case .ok: return "OK"
+        case .cancel: return "Cancel"
+        case .settings: return "Settings"
+        case .yes: return "YES"
+        case .no: return "NO"
+        case .delete: return "Remove"
+        }
+    }
+}
+
+//  Alert Messages Here----------
+enum Messages {
+    case appName
+    case custom(String)  // Here you can show custome msg like server messages here
+    case passwordMatch
+    
+    var value: String {
+        switch self {
+        case .appName : return "Yuppy"
+        case .custom(let message) : return message
+        case.passwordMatch : return "Incorrect password"
+            
+        }
+    }
+}
+
+// Alert Extension for viewwcontroller---------------------------
+
+extension UIViewController {
+    
+    func presentAlertWithTitle(title: Messages = .appName, message: Messages, options: AlertButtonTitle..., completion: CompletionHandler = nil) {
+        let alertController = UIAlertController(title: title.value, message: message.value, preferredStyle: .alert)
+        
+        for (index, option) in options.enumerated() {
+            alertController.addAction(UIAlertAction.init(title: option.value, style: (options.count > 1 ? (option != .cancel ? .destructive : .cancel) : .default), handler: { (action) in
+                if completion != nil{
+                    completion!(index)
+                }
+            }))
+        }
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func witAction(_ sender: Any) {
+        usageOfAlert()
+    }
+    
+    @IBAction func WithouAction(_ sender: Any) {
+        self.presentAlertWithTitle(message: .custom(""), options: .ok) // 1
 
+    }
+    func usageOfAlert(){
+        
+        self.presentAlertWithTitle(title: .appName, message: .passwordMatch, options: .ok,.no) { (option) in
+            switch (option) {
+            case 0:
+                print(0)
+            case 1:
+                print(1)
+            default:
+                break;
+            }
+        }
+    }
 
 }
 
